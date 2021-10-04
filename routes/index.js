5621c91eca6de58f18f311a53b4aaa371992f2cf
CamilaNieto-Centennial <cnietomo@my.centennialcollege.ca>
2021-10-03 21:53:45 -0400

    Partials Completed

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Home'});
+  res.render('index', {title: 'Home', page: 'home'});
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', {title: 'Home'});
+  res.render('index', {title: 'Home', page: 'home'});
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', {title: 'About Me'});
+  res.render('index', {title: 'About Me', page: 'about'});
 });
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', {title: 'Projects'});
+  res.render('index', {title: 'Projects', page: 'projects'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', {title: 'Services'});
+  res.render('index', {title: 'Services', page: 'services'});
 });
 
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', {title: 'Contact'});
+  res.render('index', {title: 'Contact', page: 'contact'});
 });
 
 module.exports = router;
